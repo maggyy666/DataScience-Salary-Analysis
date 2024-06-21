ed1c2830233237a4601f2117f2f37e621b3c64e0
maggyy666 <kczyz20@gmail.com>
2024-06-21 13:10:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# DataScience-Salary-Analysis
+ 
---
+++ main.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+root = tk.Tk()
+label1 = tk.Label(root,text="Label1")
+label2 = tk.Label(root, text="Label2")
+label1.pack()
+label2.pack()
+
+root.mainloop()
