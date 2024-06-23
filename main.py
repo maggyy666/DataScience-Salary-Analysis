71f7e2d672bfdbefc3a00a129183236bb2d2369f
maggyy666 <kczyz20@gmail.com>
2024-06-23 13:49:17 +0200

    Update main.py
    Update main.py by new plot and tile designs

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from tkinter import *
 import customtkinter as ctk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from plot_generator import generate_plot
+from plot_generator import generate_pie_chart
 
 root = ctk.CTk()
-root.geometry('1400x600')
+root.geometry('1800x800')
 ctk.set_appearance_mode('system')
 ctk.set_default_color_theme('blue')
 
@@ -21,21 +24,39 @@ main_frame = ctk.CTkFrame(root,corner_radius=0,fg_color='#1E1E1E')
 main_frame.grid(row=0,column=1,sticky='nsew')
 
 #grid @mainframe
-main_frame.grid_rowconfigure(0,weight=1) #top spacer
-main_frame.grid_rowconfigure(1,weight=10) #bottom spacer
-main_frame.grid_rowconfigure(2,weight=1) #middle row
-
 main_frame.grid_columnconfigure(0,weight=1) #left spacer
-main_frame.grid_columnconfigure(1,weight=0) #middle
-main_frame.grid_columnconfigure(2,weight=1) #right spacer
+main_frame.grid_columnconfigure(1,weight=0) #Tile 1
+main_frame.grid_columnconfigure(2,weight=1) #Spacer between
+main_frame.grid_columnconfigure(3,weight=10) #Tile 2
+main_frame.grid_columnconfigure(4,weight=1) #Right Spacer
+
+main_frame.grid_rowconfigure(0,weight=1) #Top spacer
+main_frame.grid_rowconfigure(1,weight=10)#Tiles row
+main_frame.grid_rowconfigure(2,weight=1) # Bottom spacer
 
 #tiles
-tile1 = ctk.CTkFrame(main_frame,fg_color='#2C2C2C')
-tile2 = ctk.CTkFrame(main_frame,fg_color='#2C2C2C')
+tile1 = ctk.CTkFrame(main_frame, fg_color='#2C2C2C')
+tile1.grid(row=1,column=1,padx=(10,5),pady=(10,5),sticky='nsew')
 
-#Tiles w/ padding
-tile1.grid(row=1, column=0, padx=(20,10), pady=(20,10), sticky='nsew')
-tile2.grid(row=1, column=2, padx=(10,20), pady=(20,10), sticky='nsew')
+tile2 = ctk.CTkFrame(main_frame,fg_color='#2C2C2C')
+tile2.grid(row=1,column=3,padx=(5,10),pady=(10,5),sticky='nsew')
+
+#plot in tiles
+def add_plot(tile):
+    fig = generate_plot()
+    canvas = FigureCanvasTkAgg(fig,tile)
+    canvas.draw()
+    canvas.get_tk_widget().pack(fill=BOTH,expand=True,padx=20,pady=20)
+add_plot(tile1)
+
+def add_pie_chart(tile):
+    fig = generate_pie_chart()
+    canvas = FigureCanvasTkAgg(fig,tile)
+    canvas.draw()
+    canvas.get_tk_widget().pack(fill=BOTH,expand=True)
+add_pie_chart(tile2)
+
+
 
 #top spacers / buttons
 top_spacer = ctk.CTkFrame(side_frame,fg_color='#2C2C2C')
@@ -53,4 +74,5 @@ for text in buttons:
     button.grid(pady=5)
 bottom_spacer = ctk.CTkFrame(side_frame, fg_color='#2C2C2C')
 bottom_spacer.grid(row=len(buttons)+1,column=0,sticky='ew')
+#run
 root.mainloop()
