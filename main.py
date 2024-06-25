e8407efcd51ca6bfb361491fece5de2d146ea6cc
maggyy666 <kczyz20@gmail.com>
2024-06-25 15:56:24 +0200

    style for plots

--- main.py
+++ main.py
@@ -124,6 +124,29 @@ def update_plot2_with_common_positions():
     plt.gca().set_aspect('equal')
     plot_on_tile(fig, tile2)
 
+def style_plot(fig, ax, title, xlabel):
+    ax.set_facecolor('#1E1E1E')
+    fig.patch.set_facecolor('#2C2C2C')
+    ax.spines['bottom'].set_color('white')
+    ax.spines['top'].set_color('white')
+    ax.spines['right'].set_color('white')
+    ax.spines['left'].set_color('white')
+    ax.tick_params(axis='x', colors='white')
+    ax.tick_params(axis='y', colors='white')
+    ax.set_xlabel(xlabel, fontsize=12, color='white', wrap=True)
+    ax.set_title(title, fontsize=10, color='white')
+    for label in ax.get_yticklabels():
+        label.set_fontsize(8)
+        label.set_color('white')
+    for label in ax.get_xticklabels():
+        label.set_fontsize(8)
+        label.set_color('white')
+    ax.grid(True, axis='x', color='gray', linestyle='--', linewidth=0.5)
+
+def plot_on_tile(fig, tile):
+    for widget in tile.winfo_children():
+        widget.destroy()
+    canvas = FigureCanvasTkAgg(fig, master=tile)
     canvas.draw()
     canvas.get_tk_widget().pack(fill=BOTH, expand=True, padx=20, pady=20)
 
