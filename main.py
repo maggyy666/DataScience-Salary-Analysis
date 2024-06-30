2e4e6fb2bccef508201b97e901207eb1275a44f7
maggyy666 <kczyz20@gmail.com>
2024-06-30 13:11:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ from tkinter import *
 import customtkinter as ctk
 from plot_generator import (
     update_plot1_with_job_titles, update_plot1_with_experience_salary,
-    update_plot1_with_job_locations, update_plot2_with_common_positions,
-    update_plot2_with_country_distribution, update_plot2_with_experience_distribution,
-    update_plot2_with_company_size_distribution, update_plot_with_category
+    update_plot1_with_job_locations, update_plot2_with_common_positions, update_plot2_with_experience_distribution,
+    update_plot2_with_company_size_distribution, update_plot2_with_employment_type_distribution,
+    update_plot_with_category
 )
 
 current_section = 'home'
 current_plot_type = 'bar'
 current_data_set = 'Average Salary'
 tile1 = None
-tile2 = None
 
 root = ctk.CTk()
 root.geometry('1800x800')
@@ -35,100 +34,31 @@ navbar.grid_columnconfigure(6, weight=1)
 navbar.grid_columnconfigure(7, weight=0)
 navbar.grid_columnconfigure(8, weight=0)
 navbar.grid_columnconfigure(9, weight=0)
-navbar.grid_columnconfigure(10, weight=1)
+navbar.grid_columnconfigure(10, weight=0)
+navbar.grid_columnconfigure(11, weight=1)
+
 
 navbar_label = ctk.CTkLabel(navbar, text="DS Salary Analysis App", fg_color='#333333', font=('Helvetica', 16, 'bold'))
 navbar_label.grid(row=0, column=0, padx=10, pady=10, sticky='w')
 
+
 def update_plot():
-    if current_section == 'home':
-        update_plot1_with_job_titles(tile1)
-        update_plot2_with_common_positions(tile2)
-    else:
-        update_plot_with_category(tile1, current_plot_type, current_section, current_data_set)
+    update_plot_with_category(tile1, current_plot_type, current_section, current_data_set)
 
-# FUNCTIONS @JOB_TITLE
-def create_job_buttons():
-    global job_button_frame
-    job_button_frame = ctk.CTkFrame(navbar, fg_color='#2C2C2C')
-    job_button_frame.grid(row=1, column=0, columnspan=11, sticky='ew')
-
-    # Configure columns for centering buttons
-    job_button_frame.grid_columnconfigure(0, weight=1)
-    job_button_frame.grid_columnconfigure(1, weight=0)
-    job_button_frame.grid_columnconfigure(2, weight=0)
-    job_button_frame.grid_columnconfigure(3, weight=0)
-    job_button_frame.grid_columnconfigure(4, weight=0)
-    job_button_frame.grid_columnconfigure(5, weight=0)
-    job_button_frame.grid_columnconfigure(6, weight=1)
-
-    avg_salary_button = ctk.CTkButton(job_button_frame, text='Average Salary', command=lambda: switch_job_data_set('Average Salary'))
-    avg_salary_button.grid(row=0, column=1, padx=10, pady=5)
-
-    salary_experience_button = ctk.CTkButton(job_button_frame, text='Salary by Experience', command=lambda: switch_job_data_set('Salary by Experience'))
-    salary_experience_button.grid(row=0, column=2, padx=10, pady=5)
-
-    job_location_button = ctk.CTkButton(job_button_frame, text='Jobs by Location', command=lambda: switch_job_data_set('Jobs by Location'))
-    job_location_button.grid(row=0, column=3, padx=10, pady=5)
-
-    experience_distribution_button = ctk.CTkButton(job_button_frame, text='Experience Distribution', command=switch_to_experience_distribution)
-    experience_distribution_button.grid(row=0, column=4, padx=10, pady=5)
-
-    company_size_distribution_button = ctk.CTkButton(job_button_frame, text='Company Size Distribution', command=switch_to_company_size_distribution)
-    company_size_distribution_button.grid(row=0, column=5, padx=10, pady=5)
-
-
-
-def show_job_buttons():
-    home_button.grid_remove()
-    experience_salary_button.grid_remove()
-    job_location_button.grid_remove()
-    create_job_buttons()
 
 def switch_job_data_set(data_set):
     global current_data_set
     current_data_set = data_set
     update_plot()
 
+
 # Functions for switching sections
 def switch_to_home():
     global current_section, current_data_set
     current_section = 'home'
     current_data_set = 'Average Salary'
-    update_plot1_with_job_titles(tile1)
-    update_plot2_with_common_positions(tile2)
+    update_plot()
     current_section_label.configure(text='Home')
-
-def switch_to_home_avg_salary():
-    global current_data_set
-    current_data_set = 'Average Salary'
-    update_plot1_with_job_titles(tile1)
-
-def switch_to_home_experience_salary():
-    global current_data_set
-    current_data_set = 'Salary by Experience'
-    update_plot1_with_experience_salary(tile1)
-
-def switch_to_home_job_location():
-    global current_data_set
-    current_data_set = 'Jobs by Location'
-    update_plot1_with_job_locations(tile1)
-
-def switch_to_home_country_distribution():
-    global current_data_set
-    current_data_set = 'Country Distribution'
-    update_plot2_with_country_distribution(tile2)
-
-def switch_to_home_experience_distribution():
-    global current_data_set
-    current_data_set = 'Experience Distribution'
-    update_plot2_with_experience_distribution(tile2)
-
-def switch_to_home_company_size_distribution():
-    global current_data_set
-    current_data_set = 'Company Size Distribution'
-    update_plot2_with_company_size_distribution(tile2)
-
 
 
 def switch_to_experience_salary():
@@ -136,39 +66,42 @@ def switch_to_experience_salary():
     current_data_set = 'Salary by Experience'
     update_plot()
 
+
 def switch_to_job_location():
     global current_data_set
     current_data_set = 'Jobs by Location'
     update_plot()
 
-def switch_to_job_titles():
+
+def switch_to_employment_type_distribution():
     global current_data_set
-    current_data_set = 'Average Salary'
+    current_data_set = 'Employment Type Distribution'
     update_plot()
 
-def switch_to_country_distribution():
-    global current_data_set
-    current_data_set = 'Country Distribution'
-    update_plot()
 
 def switch_to_experience_distribution():
     global current_data_set
     current_data_set = 'Experience Distribution'
     update_plot()
 
+
 def switch_to_company_size_distribution():
     global current_data_set
     current_data_set = 'Company Size Distribution'
     update_plot()
 
-# PIE CHART BUTTONS - shifted to the right
-country_distribution_button = ctk.CTkButton(navbar, text='Country Distribution', command=switch_to_country_distribution)
-country_distribution_button.grid(row=0, column=7, padx=(0, 10), pady=5, sticky='e')
 
-experience_distribution_button = ctk.CTkButton(navbar, text='Experience Distribution', command=switch_to_experience_distribution)
+# PIE CHART BUTTONS - shifted to the right
+employment_type_distribution_button = ctk.CTkButton(navbar, text='Employment Type Distribution',
+                                                    command=switch_to_employment_type_distribution)
+employment_type_distribution_button.grid(row=0, column=10, padx=(0, 10), pady=5, sticky='e')
+
+experience_distribution_button = ctk.CTkButton(navbar, text='Experience Distribution',
+                                               command=switch_to_experience_distribution)
 experience_distribution_button.grid(row=0, column=8, padx=(0, 10), pady=5, sticky='e')
 
-company_size_distribution_button = ctk.CTkButton(navbar, text='Company Size Distribution', command=switch_to_company_size_distribution)
+company_size_distribution_button = ctk.CTkButton(navbar, text='Company Size Distribution',
+                                                 command=switch_to_company_size_distribution)
 company_size_distribution_button.grid(row=0, column=9, padx=(0, 10), pady=5, sticky='e')
 
 # TITLE LABEL
@@ -176,14 +109,15 @@ current_section_label = ctk.CTkLabel(navbar, text='Home', fg_color='#333333', fo
 current_section_label.grid(row=0, column=5, padx=60, pady=10, sticky='w')
 
 # BAR CHART BUTTONS
-home_button = ctk.CTkButton(navbar, text='Average Salary', command=switch_to_home_avg_salary)
+home_button = ctk.CTkButton(navbar, text='Average Salary', command=switch_to_home)
 home_button.grid(row=0, column=1, padx=10, pady=5)
 
-experience_salary_button = ctk.CTkButton(navbar, text='Salary by Experience', command=switch_to_home_experience_salary)
+experience_salary_button = ctk.CTkButton(navbar, text='Salary by Experience', command=switch_to_experience_salary)
 experience_salary_button.grid(row=0, column=2, padx=10, pady=5)
 
-job_location_button = ctk.CTkButton(navbar, text='Jobs by Location', command=switch_to_home_job_location)
+job_location_button = ctk.CTkButton(navbar, text='Jobs by Location', command=switch_to_job_location)
 job_location_button.grid(row=0, column=3, padx=10, pady=5)
+
 # Sidebar
 side_frame = ctk.CTkFrame(root, width=200, height=200, corner_radius=0, fg_color='#2C2C2C')
 side_frame.grid(row=1, column=0, sticky='ns')
@@ -209,41 +143,44 @@ main_frame.grid_rowconfigure(2, weight=1)
 
 # Tiles
 tile1 = ctk.CTkFrame(main_frame, fg_color='#2C2C2C')
-tile1.grid(row=1, column=1, padx=(10, 5), pady=(10, 5), sticky='nsew')
+tile1.grid(row=1, column=2, columnspan=3, padx=(10, 5), pady=(10, 5), sticky='nsew')
 
-tile2 = ctk.CTkFrame(main_frame, fg_color='#2C2C2C')
-tile2.grid(row=1, column=3, padx=(5, 10), pady=(10, 5), sticky='nsew')
+# Frame for buttons
+button_frame = ctk.CTkFrame(main_frame, fg_color='#2C2C2C')
+button_frame.grid(row=2, column=2, columnspan=3, padx=(10, 5), pady=(10, 5), sticky='nsew')
+
+# Center the button frame within its parent
+button_frame.grid_columnconfigure(0, weight=1)
+button_frame.grid_columnconfigure(1, weight=0)
+button_frame.grid_columnconfigure(2, weight=0)
+button_frame.grid_columnconfigure(3, weight=0)
+button_frame.grid_columnconfigure(4, weight=1)
+
+# Buttons to switch between plot types
+bar_chart_button = ctk.CTkButton(button_frame, text='Bar Chart', command=lambda: switch_plot_type('bar'))
+bar_chart_button.grid(row=0, column=1, padx=10, pady=15)
+
+pie_chart_button = ctk.CTkButton(button_frame, text='Pie Chart', command=lambda: switch_plot_type('pie'))
+pie_chart_button.grid(row=0, column=2, padx=10, pady=15)
+
+box_plot_button = ctk.CTkButton(button_frame, text='Box Plot', command=lambda: switch_plot_type('box'))
+box_plot_button.grid(row=0, column=3, padx=10, pady=15)
 
 # Initially load the default plots
-switch_to_home()
+update_plot()
+
 
 # Sidebar buttons
 def clear_main_frame():
     for widget in main_frame.winfo_children():
         widget.destroy()
 
+
 def show_category_analysis(category):
     clear_main_frame()
     current_section_label.configure(text=category)
     global current_section
     current_section = category
-
-    main_frame.grid_columnconfigure(0, weight=1)
-    main_frame.grid_columnconfigure(1, weight=1)
-    main_frame.grid_columnconfigure(2, weight=1)
-    main_frame.grid_columnconfigure(3, weight=1)
-    main_frame.grid_columnconfigure(4, weight=1)
-    main_frame.grid_columnconfigure(5, weight=1)
-    main_frame.grid_columnconfigure(6, weight=1)
-
-    main_frame.grid_rowconfigure(0, weight=1)
-    main_frame.grid_rowconfigure(1, weight=1)
-    main_frame.grid_rowconfigure(2, weight=1)
-    main_frame.grid_rowconfigure(3, weight=1)
-
-    country_distribution_button.grid_remove()
-    experience_distribution_button.grid_remove()
-    company_size_distribution_button.grid_remove()
 
     # Frame for plot
     global tile1
@@ -273,7 +210,6 @@ def show_category_analysis(category):
 
     # Default plot
     update_plot_with_category(tile1, 'bar', category, current_data_set)
-    show_job_buttons()
 
 
 def switch_plot_type(plot_type):
@@ -332,7 +268,6 @@ job_titles = [
     ('Miscellaneous', lambda: show_category_analysis('Miscellaneous'))
 ]
 
-
 for i, (title, func) in enumerate(job_titles):
     title_button = ctk.CTkButton(
         job_titles_frame,
@@ -365,3 +300,5 @@ side_frame.grid_rowconfigure(6, weight=1)
 
 # Run
 root.mainloop()
+
+
